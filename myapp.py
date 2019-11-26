bbd73ccdc5207c35c92adadc933b1bf84451b31d
jorymorrison <jorymorrison@users.noreply.github.com>
2019-11-26 17:14:27 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,7 +1,11 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-g", "--greeting")
-parser.add_argument("-n", "--name")
+parser.add_argument("greeting", help="greeting")
+parser.add_argument("-n", "--name", help="name of the person you are greeting")
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 args = parser.parse_args()
-print(args.greeting + " " + args.name)
+
+if (args.name):
+    print(args.greeting + " " + args.name)
+else:
+    print(args.greeting)
